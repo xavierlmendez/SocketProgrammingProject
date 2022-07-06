4e3e5a7de77a0a7d62a15a351d1d42a81b5d8938
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2022-07-06 15:30:14 -0500

    Update server.c

--- server.c
+++ server.c
@@ -1,147 +1,132 @@
-#include<stdio.h>
+#include<pthread.h>
+#ifndef MAX
+#define MAX 900
+#endif
+void * func(void* p_client_sock){
+    int client_sock = *((int*)p_client_sock);
+    int read_size, socket_desc;
+    char *client_message = NULL;
+    client_message = malloc (2000* sizeof client_message);
+    free(p_client_sock);
+    while ((read_size = recv(client_sock, client_message, 2000, 0)) > 0) {
+        //Send the message back to client
+        printf("worker %d: received message from client %d\n", getpid(), getppid());
+        printf("received this message: %s\n", client_message);
+        int messageWordC = wordCount(client_message);
+        int messageCharC = charCount(client_message)-1;
 
-#include<stdlib.h>
+        char str1[100];
+        strcpy(str1, client_message);
+        //============================================
+        char *newmessage;
+        char buffer[MAX];
 
-#include<string.h>	//strlen
+        if (asprintf(&newmessage, " %d %d", messageWordC, messageCharC) == -1) {
+            perror("asprintf");
 
-#include<sys/socket.h>
+        } else {
+            strcat(strcpy(buffer, str1), newmessage);
+            free(newmessage);
+        }
 
-#include<arpa/inet.h>	//inet_addr
+        //=============================================
+        printf("sending back this message: %s\n", buffer);
+        write(client_sock, buffer, strlen(buffer));
+        memset(client_message, 0, sizeof client_message);
+        messageWordC = 0;
+        messageCharC = 0;
+        char check[] = "exit";
+        if((strcmp(check, client_message))==0){
+            return NULL;
+        }
+        if (read_size == 0) {
+            printf("worker %d: disconnected from client %d\n", getpid(), getppid());
+            fflush(stdout);
+            return NULL;
+        } else if (read_size == -1) {
+            perror("recv failed");
+            return NULL;
+        }
+    }
+}
+int wordCount(const char sentence[ ]){
+    int count = 0;
+    const char* str = sentence;
+    int inword = 0;
 
-#include<unistd.h>	//write
+    do switch(*str) {
+            case '\0':
+            case ' ': case '\t': case '\n': case '\r':
+                if (inword) { inword = 0; count++; }
+                break;
+            default: inword = 1;
+        } while(*str++);
 
+    return count;
+}
 
-
+int charCount(const char sentence[]){
+    int count = 0;
+    const char* str = sentence;
+    int i;
+    for(i = 0; str[i] != '\0'; i++){
+        if(str[i]!=' '){
+            count++;
+        }
+    }
+    return count;
+}
 
 int main(int argc , char *argv[])
-
 {
-
-    int socket_desc , client_sock , c , read_size;
-
+    int socket_desc , client_sock , c;
     struct sockaddr_in server , client;
 
-    char *client_message = NULL;
-
-    client_message = malloc (2000* sizeof client_message);
-
-
-
-
     //Create socket
-
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-
     if (socket_desc == -1)
-
     {
-
         printf("Could not create socket");
-
     }
-
     puts("Socket created");
-
-
-
-
-    //Prepare the sockaddr_in structure
-
+//Prepare the sockaddr_in structure
     short port = *argv[1];
-
     printf("%u\n", (short)port);
-
     server.sin_family = AF_INET;
-
     server.sin_addr.s_addr = INADDR_ANY;
-
     server.sin_port = port;
 
-
-
-
     //Bind
-
     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-
     {
-
         //print the error message
-
         perror("bind failed. Error");
-
         return 1;
-
     }
-
     puts("bind done");
-
     int cont = 0;
-
     while(cont == 0) {
-
         //Listen
-
         listen(socket_desc, 3);
-
-
-
 
         //Accept and incoming connection
 
-
-
-
         c = sizeof(struct sockaddr_in);
 
-
-
-
         //accept connection from an incoming client
-
         client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t * ) & c);
-
         if (client_sock < 0) {
-
             perror("accept failed");
-
             return 1;
-
         }
-
-        printf("worker %d: established connection with client %d#%d \n", client_sock, socket_desc, port);
-
+        printf("worker %d: established connection with client %d#%d \n", getpid(), getppid(), port);
         //Receive a message from client
+        pthread_t t;
+        int *pclient = malloc(sizeof(int));
+        *pclient = client_sock;
+        pthread_create(&t, NULL, func, pclient);
 
-        while ((read_size = recv(client_sock, client_message, 2000, 0)) > 0) {
-
-            //Send the message back to client
-
-            printf("worker %d: received message from client %d", client_sock, socket_desc);
-
-            write(client_sock, client_message, strlen(client_message));
-
-            memset(client_message, 0, sizeof client_message);
-
-        }
-
-
-
-
-        if (read_size == 0) {
-
-            printf("worker %d: disconnected from client %d", client_sock, socket_desc);
-
-            fflush(stdout);
-
-        } else if (read_size == -1) {
-
-            perror("recv failed");
-
-        }
 
     }
-
     return 0;
-
 }
